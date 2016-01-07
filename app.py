bc65fe06bf7c4b49012404532e922293c4c79d83
madhav <maddy4chat@gmail.com>
2016-01-07 16:58:33 +0530

    flask-hello-world

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/home/madhav/.virtualenvs/flask/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/hello")
+
+def hello_world():
+    return "Hello, World Folks"
+
+if __name__ == "__main__":
+    app.run()
+
