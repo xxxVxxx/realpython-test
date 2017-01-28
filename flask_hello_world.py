342d8b1c31c1da6056249cf9735a0966c73372e4
xxxVxxx <maddy4chat@gmail.com>
2017-01-28 23:53:49 +0530

    added a dynamic route

--- flask_hello_world.py
+++ flask_hello_world.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return "greetings from maddy's world of mac"
 
+@app.route('/test/<search_query>')
+def search(search_query):
+    return "Hello.. you searched for {0}".format(search_query)
+
+
 if __name__ == "__main__":
     app.run()
